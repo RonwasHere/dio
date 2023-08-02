7a5415b6cf5104a65d762f34ec6c62940e9120b0
ronald <ronaldliansugihartono@gmail.com>
2023-08-02 13:48:45 -0700

    feat = add theme.dart u/ color

---
+++ lib/theme.dart/theme_file.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+Color kBrownColor = Color(0xff70421e);
+Color kWhiteColor = Color(0xffedf2f4);
+Color kOrangeColor = Color(0xffff5002);
+Color kGreyColor = Color(0xff6c757d);
+Color kBlackColor = Color(0xff0b090a);
